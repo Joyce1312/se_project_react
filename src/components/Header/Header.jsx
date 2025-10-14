21fe8fd8d1cf64da904309b69523264f57343d82
Joyce1312 <joyce1312183j@gmail.com>
2025-10-14 17:31:24 -0400

    feat: Add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.svg";
+
+function Header() {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+  return (
+    <header className="header">
+      <img src={logo} alt="logo" className="header__logo" />
+      <p className="header__date-and-location">{currentDate}, New York</p>
+      <button
+        type="submit"
+        aria-label="Add Clothes Button"
+        className="header__add-clothes-btn"
+      >
+        + Add Clothes
+      </button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
