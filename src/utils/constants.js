f8f08d0d2ee64f69942673268fb3cc2bb879cb06
Joyce1312 <joyce1312183j@gmail.com>
2025-10-16 01:45:41 -0400

    feat: Generating WeatherCard dynamically

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,75 @@
+const weatherOptions = [
+  {
+    day: true,
+    condition: "clear",
+    url: new URL("../assets/day/clear.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "cloudy",
+    url: new URL("../assets/day/cloudy.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "fog",
+    url: new URL("../assets/day/fog.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "rain",
+    url: new URL("../assets/day/rain.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "snow",
+    url: new URL("../assets/day/snow.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "strom",
+    url: new URL("../assets/night/storm.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "clear",
+    url: new URL("../assets/night/clear.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "cloudy",
+    url: new URL("../assets/night/cloudy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "fog",
+    url: new URL("../assets/night/fog.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "rain",
+    url: new URL("../assets/night/rain.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "snow",
+    url: new URL("../assets/night/snow.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "strom",
+    url: new URL("../assets/night/storm.png", import.meta.url).href,
+  },
+];
+
+const defaultWeatherOptions = {
+  day: {
+    url: new URL("../assets/day/default.png", import.meta.url).href,
+  },
+  night: {
+    url: new URL("../assets/night/default.png", import.meta.url).href,
+  },
+};
+
 // The utils files contain default clothing items
 const defaultClothingItems = [
   {
@@ -105,4 +177,10 @@ const coordinates = {
 
 const APIkey = "311bcd48862e77981b588fde62265ff3";
 
-export { defaultClothingItems, coordinates, APIkey };
+export {
+  defaultClothingItems,
+  coordinates,
+  APIkey,
+  weatherOptions,
+  defaultWeatherOptions,
+};
